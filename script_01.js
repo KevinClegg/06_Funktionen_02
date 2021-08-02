90f376459c754cea7c4dc9546ad7c8c59b4a07a0
Kevin <kevinclegg@web.de>
2021-08-02 10:32:19 +0200

    funktion 1 + 2

--- script_01.js
+++ script_01.js
@@ -5,6 +5,32 @@
 1. Dateneingabe + -überprüfung 
 2. Auswahl Rechenart 
 3. Fkt. Grundrechenarten 
-4. Ausgabe in Konsole 
+4. Ausgabe in Konsole : Check !
 */
 
+
+
+
+
+// Modul: Addition a + b| Test :
+
+ausgabe(addieren(2,2));
+function addieren(a,b) {
+
+    const c = a + b;
+    return c ;
+    
+}
+
+
+
+
+
+
+// Modul : Konsolenausgabe | Test:
+// ausgabe("Hallo Welt");
+// ausgabe(20);
+function ausgabe(outputStr) 
+{
+    console.log(outputStr);   
+}
